9132a5b21e60484100e9c657b0e23c3cfeb223b6
CruzAPI <guilhermecruz1574@gmail.com>
2024-07-22 16:39:52 -0300

    Cancel craft/open container for fighters

--- plugin/src/main/java/com/eul4/listener/player/FighterListener.java
+++ plugin/src/main/java/com/eul4/listener/player/FighterListener.java
@@ -4,11 +4,17 @@ import com.eul4.Main;
 import com.eul4.model.player.Fighter;
 import com.eul4.model.town.Town;
 import lombok.RequiredArgsConstructor;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Tag;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.TileState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.util.BoundingBox;
 
@@ -44,6 +50,44 @@ public class FighterListener implements Listener
 		boundingBox.expandDirectional(0.0D, playerBoundingBox.getHeight(), 0.0D);
 		
 		if(!town.getBoundingBoxExcludingWalls().contains(boundingBox))
+		{
+			event.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void cancelCraft(CraftItemEvent event)
+	{
+		if(!(event.getWhoClicked() instanceof Player player)
+				|| !(plugin.getPlayerManager().get(player) instanceof Fighter fighter))
+		{
+			return;
+		}
+		
+		event.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void cancelOpenContainers(PlayerInteractEvent event)
+	{
+		Player player = event.getPlayer();
+		
+		if(!(plugin.getPlayerManager().get(player) instanceof Fighter fighter))
+		{
+			return;
+		}
+		
+		Block clickedBlock = event.getClickedBlock();
+		Action action = event.getAction();
+		
+		if(clickedBlock == null || !action.isRightClick())
+		{
+			return;
+		}
+		
+		BlockState blockState = clickedBlock.getState();
+		
+		if(blockState instanceof TileState || Tag.ANVIL.isTagged(clickedBlock.getType()))
 		{
 			event.setCancelled(true);
 		}
